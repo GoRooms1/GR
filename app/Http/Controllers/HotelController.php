9cf9fbc3bbaefbba65e60a97793cc9a6c4856e0f
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-11 16:28:37 +0700

    Tabs

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -3,35 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hotel;
-use App\Models\Review;
-use Illuminate\Http\Request;
-use \Illuminate\View\View;
 use App\Traits\Breadcrumbs;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
 class HotelController extends Controller
 {
-       use Breadcrumbs;
-    
-    public function index(Request $request): View
-    {
-        $Breadcrumbs_din = $this->get_bread();
-    
-        $hotels = Hotel::paginate(18);
-        return view('hotel.index', compact('hotels', 'request','Breadcrumbs_din'));
-    }
+  use Breadcrumbs;
 
-    public function show(Hotel $hotel, Request $request): View
-    {
-         $Breadcrumbs_din = $this->get_bread();
-        
-        $pageAbout = $hotel->meta ?? new class {
-            public $title = null;
-            public $meta_description = null;
-        };
+  public function index(Request $request): View
+  {
+    $Breadcrumbs_din = $this->get_bread();
 
-        $pageAbout->title ??= sprintf('Отель "%s" - бронь номера на час ▶Gorooms', $hotel->name);
+    $hotels = Hotel::paginate(18);
+    return view('hotel.index', compact('hotels', 'request', 'Breadcrumbs_din'));
+  }
 
-        $pageAbout->meta_description ??= sprintf('Забронируйте номер в отеле на час (сутки) "%s" онлайн в компании Gorooms ▶ Без комиссий и посредников▶ Качественное обслуживание ▶ Низкие цены ▶ Звоните!', $hotel->name);
+  public function show(Hotel $hotel, Request $request): View
+  {
+    $Breadcrumbs_din = $this->get_bread();
 
-        return view('hotel.show', compact('hotel', 'request', 'pageAbout','Breadcrumbs_din'));
-    }
+    $pageAbout = $hotel->meta ?? new class {
+        public $title = null;
+        public $meta_description = null;
+      };
+
+    $pageAbout->title ??= sprintf('Отель "%s" - бронь номера на час ▶Gorooms', $hotel->name);
+
+    $pageAbout->meta_description ??= sprintf('Забронируйте номер в отеле на час (сутки) "%s" онлайн в компании Gorooms ▶ Без комиссий и посредников▶ Качественное обслуживание ▶ Низкие цены ▶ Звоните!', $hotel->name);
+
+    return view('hotel.show', compact('hotel', 'request', 'pageAbout', 'Breadcrumbs_din'));
+  }
 }
