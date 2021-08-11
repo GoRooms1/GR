f73edc2888277c09fd9b75253fec0b7816b312ec
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-11 16:27:53 +0700

    Fix show image in windows system

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,16 +8,38 @@ use League\Glide\ServerFactory;
 
 class ImageController extends Controller
 {
-    public function show(Filesystem $filesystem, $path)
-    {
-        $server = ServerFactory::create([
-            'response' => new LaravelResponseFactory(app('request')),
-            'source' => $filesystem->getDriver(),
-            'cache' => $filesystem->getDriver(),
-            'cache_path_prefix' => '.cache',
-            'base_url' => 'image',
-        ]);
+//  public function show(Filesystem $filesystem, $path)
+//  {
+//    $server = ServerFactory::create([
+//      'response' => new LaravelResponseFactory(app('request')),
+//      'source' => $filesystem->getDriver(),
+//      'cache' => $filesystem->getDriver(),
+//      'cache_path_prefix' => '.cache',
+//      'base_url' => 'image',
+//    ]);
+////        $path = str_replace('image', 'storage', $path);
+//    return $server->getImageResponse($path, request()->all());
+//  }
 
-        return $server->getImageResponse($path, request()->all());
-    }
+  public function show($path)
+  {
+//    $server = ServerFactory::create([
+//      'response' => new LaravelResponseFactory(app('request')),
+//      'source' => $filesystem->getDriver(),
+//      'cache' => $filesystem->getDriver(),
+//      'cache_path_prefix' => '.cache',
+//      'base_url' => 'image',
+//    ]);
+
+    $server = ServerFactory::create([
+      'source' => 'storage',
+      'cache' => 'storage',
+      'cache_path_prefix' => '.cache',
+    ]);
+//    $path = str_replace('image', 'storage', $path);
+    $server->outputImage($path, request()->all());
+
+//        $path = str_replace('image', 'storage', $path);
+//    return $server->getImageResponse($path, request()->all());
+  }
 }
