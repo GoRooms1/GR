e7d0a7fcb0205976dc5fe5960df88b1544188e01
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-19 17:00:30 +0700

    Image Linux

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,8 +10,8 @@ class ImageController extends Controller
   public function show($path): void
   {
     $server = ServerFactory::create([
-      'source' => 'storage',
-      'cache' => 'storage',
+      'source' => config('app.glide_path'),
+      'cache' => config('app.glide_path'),
       'cache_path_prefix' => '.cache',
     ]);
     $server->outputImage($path, request()->all());
