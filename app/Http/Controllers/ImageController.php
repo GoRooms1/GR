ea46e3f64a488ad5c00652006f5279652e30d0ac
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-21 13:27:20 +0700

    PHPDoc

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -1,13 +1,26 @@
 <?php
+/*
+ * Copyright (c) 2021.
+ * This code is the property of the Fulliton developer.
+ * Write all questions and suggestions on the Vkontakte social network https://vk.com/fulliton
+ */
 
 namespace App\Http\Controllers;
 
 use League\Glide\ServerFactory;
 
+/**
+ * Controller for caching and image optimization
+ */
 class ImageController extends Controller
 {
 
-  public function show($path): void
+  /**
+   * Cache optimized image
+   * return Image
+   * @param string $path
+   */
+  public function show(string $path): void
   {
     $server = ServerFactory::create([
       'source' => config('cache.glide_path'),
