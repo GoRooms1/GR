ecc646d36530e12bbac67bd2a0a6ce6eeb5253d9
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-22 23:20:39 +0700

    Tabs

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -11,83 +11,83 @@ use Illuminate\Support\Str;
 
 class Address extends Model
 {
-    use ClearValidated;
+  use ClearValidated;
 
-    protected $fillable = [
-        'postal_code',
-        'country',
-        'region',
-        'area',
-        'city',
-        'city_district',
-        'street',
-        'house',
-        'block',
-        'flat',
-        'office',
-        'geo_lat',
-        'geo_lon',
-        'value',
-        'street_type',
-        'city_area',
-        'street_with_type',
-        'comment'
-    ];
+  protected $fillable = [
+    'postal_code',
+    'country',
+    'region',
+    'area',
+    'city',
+    'city_district',
+    'street',
+    'house',
+    'block',
+    'flat',
+    'office',
+    'geo_lat',
+    'geo_lon',
+    'value',
+    'street_type',
+    'city_area',
+    'street_with_type',
+    'comment'
+  ];
 
-    public function hotel(): BelongsTo
-    {
-        return $this->belongsTo(Hotel::class);
+  public static function boot()
+  {
+    parent::boot();
+
+    self::creating(function ($model) {
+      self::setAddressesSlug($model);
+    });
+
+    self::updating(function ($model) {
+      self::setAddressesSlug($model);
+    });
+    self::deleting(function (self $address) {
+      Cache::forget('sitemap.2g');
+    });
+  }
+
+  public static function setAddressesSlug($model)
+  {
+    $slugs = Address::getSlugFromAddress($model);
+    Cache::forget('sitemap.2g');
+    foreach ($slugs as $slug)
+      DB::table('address_slug')->updateOrInsert(['address' => $slug['address']], $slug);
+  }
+
+  public static function getSlugFromAddress(Address $address)
+  {
+    $columns = ['region', 'area', 'city', 'city_district', 'street', 'city_area'];
+
+    $slugs = [];
+    foreach ($columns as $column) {
+      $attribute = $address->getAttribute($column);
+      if (!is_null($attribute) && !empty($attribute)) {
+        $slugs[] = [
+          'address' => $attribute,
+          'slug' => Str::slug($attribute)
+        ];
+      }
     }
 
-    public static function getSlugFromAddress(Address $address)
-    {
-        $columns = ['region', 'area', 'city', 'city_district', 'street', 'city_area'];
+    return $slugs;
+  }
 
-        $slugs = [];
-        foreach ($columns AS $column) {
-            $attribute = $address->getAttribute($column);
-            if (!is_null($attribute) && !empty($attribute)) {
-                $slugs[] = [
-                    'address' => $attribute,
-                    'slug' => Str::slug($attribute)
-                ];
-            }
-        }
+  public function hotel(): BelongsTo
+  {
+    return $this->belongsTo(Hotel::class);
+  }
 
-        return $slugs;
-    }
-
-    public static function setAddressesSlug($model)
-    {
-        $slugs = Address::getSlugFromAddress($model);
-        Cache::forget('sitemap.2g');
-        foreach ($slugs AS $slug)
-            DB::table('address_slug')->updateOrInsert(['address' => $slug['address']], $slug);
-    }
-
-    public static function boot()
-    {
-        parent::boot();
-
-        self::creating(function($model) {
-            self::setAddressesSlug($model);
-        });
-
-        self::updating(function($model) {
-            self::setAddressesSlug($model);
-        });
-        self::deleting(function(self $address) {
-            Cache::forget('sitemap.2g');
-        });
-    }
-
-    public function getCityAreaShortAttribute()
-    {
-        $areas = explode('-', $this->city_area);
-        $area = '';
-        foreach ($areas AS $area_prefix)
-            $area .= mb_substr($area_prefix, 0, 1);
-        $area = mb_strtoupper($area).'АО';
-        return $area;
-    }
+  public function getCityAreaShortAttribute()
+  {
+    $areas = explode('-', $this->city_area);
+    $area = '';
+    foreach ($areas as $area_prefix)
+      $area .= mb_substr($area_prefix, 0, 1);
+    $area = mb_strtoupper($area) . 'АО';
+    return $area;
+  }
 }
