cc5d60c092e359701a80d5bc3dc419bc9484ec46
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-24 23:51:41 +0700

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,36 +4,37 @@ namespace App\Providers;
 
 use App\Events\FormSend;
 use App\Listeners\FormSendMailEvent;
+use App\Models\Image;
+use App\Observers\ImageObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-        FormSend::class => [
-            FormSendMailEvent::class,
-        ]
-    ];
+  /**
+   * The event listener mappings for the application.
+   *
+   * @var array
+   */
+  protected $listen = [
+    Registered::class => [
+      SendEmailVerificationNotification::class,
+    ],
+    FormSend::class => [
+      FormSendMailEvent::class,
+    ]
+  ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
+  /**
+   * Register any events for your application.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+//        parent::boot();
+    Image::observe(ImageObserver::class);
 
-        //
-    }
+  }
 }
