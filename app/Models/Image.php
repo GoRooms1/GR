d0da9b0c2463be76366f47c533b054f68bf677be
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2021-08-24 23:51:34 +0700

    Code porting to Observe

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,11 +8,8 @@
 namespace App\Models;
 
 use App\Traits\CreatedAtOrdered;
-use Exception;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class Image extends Model
 {
@@ -31,7 +28,7 @@ class Image extends Model
     'moderate'
   ];
 
-  public static function upload($request, &$uploadTo, $attr_name = 'image')
+  public static function upload($request, &$uploadTo, $attr_name = 'image'): array
   {
     if ($request->hasFile($attr_name)) {
       $files = $request->file($attr_name);
@@ -40,15 +37,17 @@ class Image extends Model
       }
       $images = [];
       $is_default = true;
-      if ($uploadTo->image()->count())
+      if ($uploadTo->image()->count() !== 0) {
         $is_default = false;
+      }
       foreach ($files as $file) {
         $path = $file->store(date('Y/m/d'));
-        $image = Image::create([
-          'name' => $file->getClientOriginalName(),
-          'path' => 'storage/' . $path,
-          'default' => $is_default,
-        ]);
+        $image = new Image();
+        $image->name = $file->getClientOriginalName();
+        $image->path = 'storage/' . $path;
+        $image->default = $is_default;
+        $image->moderate = !(Auth::user()->is_moderate || Auth::user()->is_admin);
+        $image->save();
         $images[] = $image;
         // self::watermark($uploadTo, $image);
         $is_default = false;
@@ -62,8 +61,9 @@ class Image extends Model
 
   public static function watermark($model, Image $image): void
   {
-    if (!($model instanceof Hotel || $model instanceof Room))
+    if (!($model instanceof Hotel || $model instanceof Room)) {
       return;
+    }
 
     $path = str_replace('storage', 'app/public', $image->path);
     $img = \Intervention\Image\Facades\Image::make(storage_path($path));
@@ -71,49 +71,49 @@ class Image extends Model
     $img->save($image->path);
   }
 
-  public static function boot()
-  {
-    parent::boot();
+//  public static function boot()
+//  {
+//    parent::boot();
+//
+//    self::updating(function (Image $image) {
+//      self::beforeSave($image);
+//    });
+//
+//    self::creating(function (Image $image) {
+//      self::beforeSave($image);
+//    });
+//
+//    self::saving(function (Image $image) {
+//      self::beforeSave($image);
+//    });
+//
+//    self::deleting(function (Image $image) {
+//      if ($image->default) {
+//        $model_id = $image->model_id;
+//        $model_type = $image->model_type;
+//        try {
+//          $other_image = Image::where('model_id', $model_id)
+//            ->where('model_type', $model_type)
+//            ->whereNot('id', $image->id)
+//            ->first();
+//          $other_image->default = true;
+//          $other_image->save();
+//        } catch (Exception $exception) {
+//          if (Image::where('model_id', $model_id)->where('model_type', $model_type)->count() > 0)
+//            Log::error($exception);
+//        }
+//      }
+//      File::delete($image->getRawOriginal('path'));
+//    });
+//  }
 
-    self::updating(function (Image $image) {
-      self::beforeSave($image);
-    });
-
-    self::creating(function (Image $image) {
-      self::beforeSave($image);
-    });
-
-    self::saving(function (Image $image) {
-      self::beforeSave($image);
-    });
-
-    self::deleting(function (Image $image) {
-      if ($image->default) {
-        $model_id = $image->model_id;
-        $model_type = $image->model_type;
-        try {
-          $other_image = Image::where('model_id', $model_id)
-            ->where('model_type', $model_type)
-            ->whereNot('id', $image->id)
-            ->first();
-          $other_image->default = true;
-          $other_image->save();
-        } catch (Exception $exception) {
-          if (Image::where('model_id', $model_id)->where('model_type', $model_type)->count() > 0)
-            Log::error($exception);
-        }
-      }
-      File::delete($image->getRawOriginal('path'));
-    });
-  }
-
-  public static function beforeSave(Image $image)
+  public static function beforeSave(Image $image): void
   {
     if ($image->default) {
       $model_id = $image->model_id;
       $model_type = $image->model_type;
 
-      Image::where('model_id', $model_id)->where('model_type', $model_type)->update(['default' => false]);
+      self::where('model_id', $model_id)->where('model_type', $model_type)->update(['default' => false]);
     }
   }
 
