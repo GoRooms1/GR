f5f9b5b6b6279836dd0a8ecbbd4fea917878dd77
Viktor Niderman <viktor.n@newizze.com>
2021-09-14 22:04:31 +0300

    fix seeders

--- database/seeds/PagesSeeder.php
+++ database/seeds/PagesSeeder.php
@@ -11,25 +11,37 @@ class PagesSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Page::create([
-            'title' => 'Контакты',
-            'slug' => 'contacts',
-            'content' => 'Содержимое контактов',
-        ]);
-        \App\Models\Page::create([
-            'title' => 'Правила бронирования',
-            'slug' => 'rules',
-            'content' => 'Правила бронирования',
-        ]);
-        \App\Models\Page::create([
-            'title' => 'Бонусная программа',
-            'slug' => 'bonuse',
-            'content' => 'Бонусная программа',
-        ]);
-        \App\Models\Page::create([
-            'title' => 'Пользовательское соглашение',
-            'slug' => 'privacy-policy',
-            'content' => 'Пользовательское соглашение',
-        ]);
+        $page = \App\Models\Page::where('title', 'Контакты')->first();
+        if (!$page) {
+            \App\Models\Page::create([
+                'title' => 'Контакты',
+                'slug' => 'contacts',
+                'content' => 'Содержимое контактов',
+            ]);
+        }
+        $page = \App\Models\Page::where('title', 'Правила бронирования')->first();
+        if (!$page) {
+            \App\Models\Page::create([
+                'title' => 'Правила бронирования',
+                'slug' => 'rules',
+                'content' => 'Правила бронирования',
+            ]);
+        }
+        $page = \App\Models\Page::where('title', 'Бонусная программа')->first();
+        if (!$page) {
+            \App\Models\Page::create([
+                'title' => 'Бонусная программа',
+                'slug' => 'bonuse',
+                'content' => 'Бонусная программа',
+            ]);
+        }
+        $page = \App\Models\Page::where('title', 'Пользовательское соглашение')->first();
+        if (!$page) {
+            \App\Models\Page::create([
+                'title' => 'Пользовательское соглашение',
+                'slug' => 'privacy-policy',
+                'content' => 'Пользовательское соглашение',
+            ]);
+        }
     }
 }
