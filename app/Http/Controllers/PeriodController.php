a5514bbec89e63c56f7a5ad29d86d2e9ea2a267e
Fulliton <artyshko.andrey@gmail.com>
2021-09-24 01:12:33 +0700

    Fix Zeref

--- app/Http/Controllers/PeriodController.php
+++ app/Http/Controllers/PeriodController.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 class PeriodController extends Controller
 {
     public function updateByJson(Request $request) {
-        $new_periods = json_decode($request->result_json);
-        foreach ($new_periods->for_delete as $period_id) {
+      $new_periods = json_decode($request->result_json);
+      foreach ($new_periods->for_delete as $period_id) {
             Period::find($period_id)->delete();
         }
         foreach ($new_periods->data as $period) {
-            if ($period->id) {
+            if (isset($period->id)) {
                 Period::find($period->id)->update((array)$period);
             } else {
                 Period::create((array)$period);
