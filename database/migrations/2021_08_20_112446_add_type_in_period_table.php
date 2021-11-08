cfd357468ce7cbfb97aa16afe1ba8937233a672f
Fulliton <artyshko.andrey@gmail.com>
2021-11-08 23:43:19 +0700

    Update 2021_08_20_112446_add_type_in_period_table.php

--- database/migrations/2021_08_20_112446_add_type_in_period_table.php
+++ database/migrations/2021_08_20_112446_add_type_in_period_table.php
@@ -22,6 +22,10 @@ class AddTypeInPeriodTable extends Migration
   public function up(): void
   {
     Schema::table('periods', function (Blueprint $table) {
+      $table->dropColumn(['cost_type_id']);
+    });
+
+    Schema::table('periods', function (Blueprint $table) {
       $table->foreignId('cost_type_id')
         ->after('end_at')
         ->constrained('cost_types')
