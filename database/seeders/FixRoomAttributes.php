537dcba3c1dc573b96642323a4ccbcf277c357f9
Fulliton <artyshko.andrey@gmail.com>
2022-01-23 01:43:42 +0700

    Для копирования атрибутов из отеля в комнаты с сохранением всех данных

---
+++ database/seeders/FixRoomAttributes.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Room;
+use App\Models\Hotel;
+use App\Models\Attribute;
+use Illuminate\Database\Seeder;
+
+class FixRoomAttributes extends Seeder
+{
+
+  public function run()
+  {
+    $rooms = Room::withoutGlobalScopes()->get();
+
+    foreach ($rooms as $room) {
+      $attrs = $room->attrs;
+
+      foreach ($attrs as $attr) {
+        if ($attr->model = Hotel::class) {
+          $aNewFlag = Attribute::where('name', $attr->name)->where('model', Room::class)->exists();
+          if($aNewFlag) {
+            $id = Attribute::where('name', $attr->name)->where('model', Room::class)->first()->id;
+          } else {
+            $aNew = new Attribute([
+              'model' => Room::class,
+              'name' => $attr->name,
+              'in_filter' => $attr->in_filter,
+              'attribute_category_id' => $attr->attribute_category_id
+            ]);
+            $aNew->save();
+
+            $id = $aNew;
+          }
+
+          $room->attrs()->detach($attr->id);
+          $room->attrs()->attach($id);
+        }
+      }
+    }
+  }
+
+}
