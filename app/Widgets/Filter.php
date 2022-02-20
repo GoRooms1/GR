e32f5d7ea206e345f47d3c014116c907fdd7aede
Fulliton <artyshko.andrey@gmail.com>
2022-02-20 17:38:18 +0700

    Поддрежка красивых ссылок у фильтра

--- app/Widgets/Filter.php
+++ app/Widgets/Filter.php
@@ -7,13 +7,16 @@ use App\Models\Metro;
 use App\Models\Address;
 use App\Models\HotelType;
 use App\Models\Attribute;
+use App\Traits\UrlDecodeFilter;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Arrilot\Widgets\AbstractWidget;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Request;
 
 class Filter extends AbstractWidget
 {
+  use UrlDecodeFilter;
   /**
    * The configuration array.
    *
@@ -34,7 +37,7 @@ class Filter extends AbstractWidget
   protected Collection $hotels_type;
   protected Collection $hotels_attributes;
   protected Collection $rooms_attributes;
-  protected ?string $hotel_type;
+  protected ?string $hotel_type = '';
   protected Object $data;
 
   /**
@@ -44,55 +47,71 @@ class Filter extends AbstractWidget
   public function run()
   {
     $this->request = Request();
+    $flagRouteAddress = false;
+    if (Request::routeIs('search.address')) {
+      $flagRouteAddress = true;
 
-    if ($metro_req = Request::get('metro')) {
-      $metro = Metro::where('name', 'like', '%' . $metro_req . '%')->first();
-      if ($metro) {
-        $this->metro = $metro->name;
+      $url = $this->request->url();
+
+      $data = $this->decodeUrl($url);
+
+      $this->city = $data['city'];
+      $this->metro = $data['metro'];
+      $this->area = $data['area'];
+      $this->district = $data['district'];
+      $this->street = $data['street'];
+
+    } else {
+
+      if ($metro_req = Request::get('metro')) {
+        $metro = Metro::where('name', 'like', '%' . $metro_req . '%')->first();
+        if ($metro) {
+          $this->metro = $metro->name;
+        }
       }
+
+      if ($city_req = Request::get('city', $this->defaultLocation())) {
+        $adr = Address::where('city', 'like', '%' . $city_req . '%')->first();
+        if ($adr) {
+          $this->city = $adr->city;
+        }
+      }
+
+      if ($district_req = Request::get('district')) {
+        $adr = Address::where('city_district', 'like', '%' . $district_req . '%')->first();
+        if ($adr) {
+          $this->district = $adr->city_district;
+        }
+      }
+
+      if ($area_req = Request::get('city_area')) {
+        $adr = Address::where('city_area', 'like', '%' . $area_req . '%')->first();
+        if ($adr) {
+          $this->area = $adr->city_area;
+        }
+      }
+
+      if ($street_req = Request::get('street')) {
+        $adr = Address::where('street', 'like', '%' . $street_req . '%')->first();
+        if ($adr) {
+          $this->street = $adr->street;
+        }
+      }
+
+      $this->query = Request::get('query');
+
+      $this->hotel_type = Request::get('hotel_type');
     }
 
-    if ($city_req = Request::get('city', $this->defaultLocation())) {
-      $adr = Address::where('city', 'like', '%' . $city_req . '%')->first();
-      if ($adr) {
-        $this->city = $adr->city;
-      }
-    }
-
-    if ($district_req = Request::get('district')) {
-      $adr = Address::where('city_district', 'like', '%' . $district_req . '%')->first();
-      if ($adr) {
-        $this->district = $adr->city_district;
-      }
-    }
-
-    if ($area_req = Request::get('city_area')) {
-      $adr = Address::where('city_area', 'like', '%' . $area_req . '%')->first();
-      if ($adr) {
-        $this->area = $adr->city_area;
-      }
-    }
-
-    if ($street_req = Request::get('street')) {
-      $adr = Address::where('street', 'like', '%' . $street_req . '%')->first();
-      if ($adr) {
-        $this->street = $adr->street;
-      }
-    }
-
-    $this->query = Request::get('query');
-
-    $this->hotels_type = Cache::remember('hotels_type', 60*60*24*12, static function () {
+    $this->hotels_type = Cache::remember('hotels_type', 60 * 60 * 24 * 12, static function () {
       return HotelType::orderBy('sort')->get();
     });
 
-    $this->hotel_type = Request::get('hotel_type');
-
-    $this->hotels_attributes = Cache::remember('hotels_attributes', 60*60*24*12, static function() {
+    $this->hotels_attributes = Cache::remember('hotels_attributes', 60 * 60 * 24 * 12, static function () {
       return Attribute::forHotels()->filtered()->get();
     });
 
-    $this->rooms_attributes = Cache::remember('rooms_attributes', 60*60*24*12, static function () {
+    $this->rooms_attributes = Cache::remember('rooms_attributes', 60 * 60 * 24 * 12, static function () {
       return Attribute::forRooms()->filtered()->get();
     });
 
