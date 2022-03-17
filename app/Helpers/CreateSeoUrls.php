55324454b58116d51ec7c2db32f70bd10df8d26b
Fulliton <artyshko.andrey@gmail.com>
2022-03-17 13:36:20 +0300

    Фиксы и алгоритм обновления или создания сео для отелей

--- app/Helpers/CreateSeoUrls.php
+++ app/Helpers/CreateSeoUrls.php
@@ -3,13 +3,14 @@
 namespace App\Helpers;
 
 use Str;
+use App\Models\Hotel;
 use App\Models\Address;
 use App\Models\PageDescription;
 
 class CreateSeoUrls
 {
 
-  public function createUrlFromAddress (Address $address): void
+  public function createUrlFromAddress (Address $address, bool $override = false): void
   {
     $seo = [];
 
@@ -48,33 +49,80 @@ class CreateSeoUrls
       $seo[0] = null;
     }
 
-    $metros_name = $address->hotel->metros()->pluck('name');
-    foreach ($metros_name as $j => $name) {
-      $seo[$j + 3] = new SeoData($address);
-      $seo[$j + 3]->url = '/address/' . Str::slug($address->city) . '/metro-' . Str::slug($name);
-      $seo[$j + 3]->metro = $name;
-      $seo[$j + 3]->lastOfType = 'metro';
+    if ($address->hotel()->withoutGlobalScope('moderation')->first()->metros()->count() > 0) {
+      $metros_name = $address->hotel()->withoutGlobalScope('moderation')->first()->metros()->pluck('name');
+      foreach ($metros_name as $j => $name) {
+        $seo[$j + 3] = new SeoData($address);
+        $seo[$j + 3]->url = '/address/' . Str::slug($address->city) . '/metro-' . Str::slug($name);
+        $seo[$j + 3]->metro = $name;
+        $seo[$j + 3]->lastOfType = 'metro';
+      }
     }
 
     $seo = array_filter($seo, static fn($item): bool => $item !== null);
 
     foreach ($seo as $key => $seoData) {
-      if (!PageDescription::where('url', $seoData->url)->exists())  {
+      if ($override) {
         $seoData->generate();
-
+        $pg = PageDescription::where('url', $seoData->url)->first();
+        $description = optional($pg)->description;
+        $meta_keywords = optional($pg)->meta_keywords;
         $pageDescription = new PageDescription([
           'url' => $seoData->url,
           'title' => $seoData->title,
           'meta_description' => $seoData->description,
-          'h1' => $seoData->h1
+          'h1' => $seoData->h1,
+          'description' => $description,
+          'meta_keywords' => $meta_keywords
         ]);
+        if ($pg) {
+          $pg->delete();
+        }
         $pageDescription->save();
-
       } else {
-        $seo[$key] = null;
+        if (!PageDescription::where('url', $seoData->url)->exists()) {
+
+
+          $pageDescription = new PageDescription([
+            'url' => $seoData->url,
+            'title' => $seoData->title,
+            'meta_description' => $seoData->description,
+            'h1' => $seoData->h1
+          ]);
+          $pageDescription->save();
+
+        } else {
+          $seo[$key] = null;
+        }
       }
     }
 
     $seo = array_filter($seo, static fn($item): bool => $item !== null);
+  }
+
+  public function createUrlFromHotel (Hotel $hotel): void
+  {
+    $url = '/hotels/' . $hotel->slug;
+    $seoData = new SeoData($hotel->address, $url);
+    $seoData->lastOfType = 'hotel';
+    $seoData->hotel = $hotel;
+    $seoData->generate();
+    $description = null;
+    if ($hotel->meta) {
+      $description = $hotel->meta->description;
+      $hotel->meta->delete();
+    }
+
+    $pageDescription =  new PageDescription([
+      'url' => $seoData->url,
+      'title' => $seoData->title,
+      'meta_description' => $seoData->description,
+      'h1' => $seoData->h1,
+      'description' => $description
+    ]);
+    $pageDescription->model_type = Hotel::class;
+    $pageDescription->save();
+    $hotel->meta()->save($pageDescription);
+
   }
 }
