c4f7d471c0556797dabb4bb95c9113997d39129d
Fulliton <artyshko.andrey@gmail.com>
2022-03-17 13:36:35 +0300

    Команда для генерации сео

---
+++ app/Console/Commands/Hotels/CreateSeo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands\Hotels;
+
+use App\Models\Hotel;
+use Illuminate\Console\Command;
+
+class CreateSeo extends Command
+{
+  protected $signature = 'hotels:create-seo';
+
+  public function handle(): int
+  {
+    $address = \App\Models\Address::all();
+    $createSeo = new \App\Helpers\CreateSeoUrls();
+    foreach ($address as $a) {
+      $createSeo->createUrlFromAddress($a, true);
+    }
+
+    $hotels = Hotel::withoutGlobalScope('moderation')->get();
+    foreach ($hotels as $a) {
+      if ($a->address) {
+        $createSeo->createUrlFromHotel($a);
+      }
+    }
+    return 0;
+  }
+}
