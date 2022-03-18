20f62c6ba77cb3a86b8218f2b638464aa806db9e
Fulliton <artyshko.andrey@gmail.com>
2022-03-18 12:03:09 +0700

    ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,34 +2,64 @@
 
 namespace Tests\Feature;
 
-use App\Models\Hotel;
-use App\Models\PageDescription;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Room;
+use App\Models\Hotel;
+use App\Models\Address;
+use App\Models\PageDescription;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testHotelGenerateSlug(): void
-    {
-        $hotel = Factory::factoryForModel(Hotel::class)->create();
-        $this->assertModelExists($hotel);
+  /**
+   * A basic test example.
+   *
+   * @return void
+   */
+  public function testHotelGenerateSlug(): void
+  {
+    $hotel = Factory::factoryForModel(Hotel::class)->create();
+    $this->assertModelExists($hotel);
+  }
+
+  public function testGenerateSeoForAddress(): void
+  {
+    $hotel = Hotel::orderBy('id', 'asc')->first();
+    $countOldPageDescription = PageDescription::count();
+    $hotel->saveAddress('г Красноярск, ул Горького, д 24 кв 25');
+    $hotel->save();
+
+    $countNewPageDescription = PageDescription::count();
+
+    $this->assertNotEquals($countOldPageDescription, $countNewPageDescription);
+  }
+
+  public function testRemoveAddressAndHotelsSeo(): void
+  {
+
+    $pgs = PageDescription::where('model_type', Hotel::class)->get();
+    foreach ($pgs as $pg) {
+      if (!Hotel::withoutGlobalScope('moderation')->where('id',$pg->model_id)->exists()) {
+        $pg->delete();
+      }
     }
 
-    public function testGenerateSeoForAddress(): void
-    {
-      $hotel = Hotel::orderBy('id', 'asc')->first();
-      $countOldPageDescription = PageDescription::count();
-      $hotel->saveAddress('г Красноярск, ул Горького, д 24 кв 25');
-      $hotel->save();
+    $pgs = PageDescription::where('model_type', Room::class)->delete();
 
-      $countNewPageDescription = PageDescription::count();
 
-      $this->assertNotEquals($countOldPageDescription, $countNewPageDescription);
+    $addresses = Address::all();
+    foreach ($addresses as $address) {
+      $address->delete();
     }
+
+    $hotels = Hotel::withoutGlobalScope('moderation')->get();
+    foreach ($hotels as $hotel) {
+      $hotel->delete();
+    }
+
+    $count = PageDescription::count();
+
+    $this->assertEquals(0, $count);
+
+  }
 }
