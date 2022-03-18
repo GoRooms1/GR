779e2123334dfb27906eed69ce3eedc5dc6a1cc8
Fulliton <artyshko.andrey@gmail.com>
2022-03-18 14:16:39 +0700

    In search, view h1 or title

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
   @widget('Filter')
-
+  {{ dd($pageDescription) }}
   <section class="section">
     <div class="container">
       <div class="section-header">
-        <h1 class="section-title">{!! $pageDescription->title ?? $title !!}</h1>
+        <h1 class="section-title">{!! $pageDescription->h1 ?? $pageDescription->title ?? $title !!}</h1>
       </div>
     </div>
   </section>
