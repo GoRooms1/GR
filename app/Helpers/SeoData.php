27763f63640efc210596a0dd26aa0e0250e3fea5
Fulliton <artyshko.andrey@gmail.com>
2022-04-01 16:53:18 +0700

    Фикс создания мета описания отеля

--- app/Helpers/SeoData.php
+++ app/Helpers/SeoData.php
@@ -44,7 +44,7 @@ class SeoData
 
       $this->description = $type . ' ' . $this->hotel->name;
       foreach ($this->hotel->minimals as $minimal) {
-        if ($minimal->name === 'На час' && $minimal->value > 0) {
+        if ($minimal->name === 'На Час' && $minimal->value > 0) {
           $this->description .= 'с номерами от ' . $minimal->value . ' рублей в час';
         }
       }
