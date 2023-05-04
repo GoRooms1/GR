cfe8cb5b44eb2f7a5644a1d5fffff8e4decdf405
MikhailSergeev <michaelmorhant@gmail.com>
2023-05-04 21:34:11 +0300

    Fix hotels and rooms costs calculation.

--- src/Domain/Hotel/Actions/MinimumCostsCalculation.php
+++ src/Domain/Hotel/Actions/MinimumCostsCalculation.php
@@ -26,22 +26,27 @@ final class MinimumCostsCalculation extends Action
     {
         $result = [];
 
-        $minCosts = CostType::selectRaw('cost_types.id, cost_types.name, min(periods.start_at) as start_at, min(periods.end_at) as end_at, IFNULL(MIN(costs.value), 0) AS value')
-            ->leftJoin('periods','cost_types.id','=','periods.cost_type_id')
-            ->leftJoin('costs', function($join) use ($hotel) {
-                $join->on('costs.period_id','=', DB::raw('periods.id AND costs.value > 0'))
-                ->whereIn('costs.room_id', (function ($query) use ($hotel) {
-                    $query->from('rooms')
-                        ->select('id')
-                        ->where('hotel_id','=', $hotel->id);
-                }));                
-            })                        
-            ->groupBy('cost_types.id','cost_types.name')
-            ->orderBy('cost_types.sort','asc')
-            ->get();    
+        $minCosts = CostType::selectRaw('cost_types.id, cost_types.name, periods.start_at as start_at, periods.end_at as end_at, costs.value AS value')
+        ->leftJoin('periods','cost_types.id','=','periods.cost_type_id')
+        ->leftJoin('costs', function($join) use ($hotel) {
+            $join->on('costs.period_id','=', DB::raw('periods.id AND costs.value > 0'))
+            ->whereIn('costs.room_id', (function ($query) use ($hotel) {
+                $query->from('rooms')
+                    ->select('id')
+                    ->where('hotel_id','=', $hotel->id);
+            }));                
+        })
+        ->orderBy('cost_types.sort','asc')
+        ->get();
 
+        $minCosts = $minCosts->unique(function ($item) {
+              return $item['id'] . $item['value'];
+        })->groupBy('id')->map(function($group) {
+            return $group->firstWhere('value', $group->min('value'));
+        });
+        
         foreach ($minCosts as $minCost) {
-            $value = $minCost['value'];
+            $value = $minCost['value'] ?? 0;
             $result[] = new MinCostsData(
                 id: $minCost->id,
                 name: $minCost->name,
