719990629eb4bea809034300003e9cd742ae3926
MikhailSergeev <michaelmorhant@gmail.com>
2023-05-26 22:57:26 +0300

    Add lazy load for search result.

--- src/Domain/Search/Traits/SearchResultTrait.php
+++ src/Domain/Search/Traits/SearchResultTrait.php
@@ -15,9 +15,7 @@ use Domain\Address\DataTransferObjects\MetroSearchData;
 use Domain\Hotel\Actions\SearchHotelsAction;
 use Domain\Hotel\DataTransferObjects\HotelSearchData;
 use Domain\Search\DataTransferObjects\SearchData;
-use Spatie\LaravelData\CursorPaginatedDataCollection;
-use Spatie\LaravelData\DataCollection;
-use Spatie\LaravelData\PaginatedDataCollection;
+use Inertia\Inertia;
 
 
 /**
@@ -27,40 +25,41 @@ trait SearchResultTrait
 {
     
     /**     
-     * @return CursorPaginatedDataCollection|DataCollection|PaginatedDataCollection
+     * @return \Inertia\LazyProp
      */
-    public function search_result(): CursorPaginatedDataCollection|DataCollection|PaginatedDataCollection
-    {        
+    public function search_result(): \Inertia\LazyProp {
         $search = $this->params->search;
         
-        return SearchData::collection([
-            [
-                'title' => 'Отели',
-                'sort' => 1,
-                'blank' => true,
-                'data' => HotelSearchData::collection(SearchHotelsAction::run($search))
-            ],
-            [
-                'title' => 'Метро',
-                'sort' => 2,
-                'data' => MetroSearchData::collection(SearchMetrosAction::run($search))
-            ],
-            [
-                'title' => 'Районы',
-                'sort' => 3,
-                'data' => CityDistrictSearchData::collection(SearchCityDistrictsAction::run($search))
-            ],
-            [
-                'title' => 'Огруга',
-                'sort' => 4,
-                'data' => CityAreaSearchData::collection(SearchCityAreasAction::run($search))
-            ],
-            [
-                'title' => 'Города',
-                'sort' => 5,
-                'data' => CitySearchData::collection(SearchCitiesAction::run($search))
-            ],
-        ]);
+        return  Inertia::lazy(fn() =>
+            SearchData::collection([
+                [
+                    'title' => 'Отели',
+                    'sort' => 1,
+                    'blank' => true,
+                    'data' => HotelSearchData::collection(SearchHotelsAction::run($search))
+                ],
+                [
+                    'title' => 'Метро',
+                    'sort' => 2,
+                    'data' => MetroSearchData::collection(SearchMetrosAction::run($search))
+                ],
+                [
+                    'title' => 'Районы',
+                    'sort' => 3,
+                    'data' => CityDistrictSearchData::collection(SearchCityDistrictsAction::run($search))
+                ],
+                [
+                    'title' => 'Огруга',
+                    'sort' => 4,
+                    'data' => CityAreaSearchData::collection(SearchCityAreasAction::run($search))
+                ],
+                [
+                    'title' => 'Города',
+                    'sort' => 5,
+                    'data' => CitySearchData::collection(SearchCitiesAction::run($search))
+                ],
+            ])
+        );
     }
 
 }
