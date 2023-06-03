ddde88ae913fedd1b4b76e2b4462a8ac7899740d
MikhailSergeev <michaelmorhant@gmail.com>
2023-06-03 19:34:22 +0300

    Add lazy load for Yandex metrica.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,11 +14,11 @@
   <link rel="shortcut icon" href="{{ asset('favicon.svg') }}" type="image/x-icon">
   <link rel="apple-touch-icon" href="{{ asset('favicon.svg') }}">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="preload" as="style">  
-  <link rel="canonical" href="{{ url(Request::url()) }}"/>
- 
-  @vite  
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" as="style">
+  <link rel="canonical" href="{{ url(Request::url()) }}" />
+
+  @vite
   @inertiaHead
 </head>
 
@@ -28,23 +28,64 @@
   @if (config('app.env') === 'production')
   <!-- Yandex.Metrika counter -->
   <script type="text/javascript">
-    (function (m, e, t, r, i, k, a) {
-      m[i] = m[i] || function () {
-        (m[i].a = m[i].a || []).push(arguments)
-      };
-      m[i].l = 1 * new Date();
-      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-    ym(72285235, "init", {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true
-    });
+    (function() {
+      'use strict';
+      var loadedMetrica = false,
+        metricaId = 72285235,
+        timerId;
+
+      if (navigator.userAgent.indexOf('YandexMetrika') > -1) {
+        loadMetrica();
+      } else {
+        window.addEventListener('scroll', loadMetrica, {
+          passive: true
+        });
+        window.addEventListener('touchstart', loadMetrica);
+        document.addEventListener('mouseenter', loadMetrica);
+        document.addEventListener('click', loadMetrica);
+        document.addEventListener('DOMContentLoaded', loadFallback);
+      }
+
+      function loadFallback() {
+        timerId = setTimeout(loadMetrica, 1000);
+      }
+
+      function loadMetrica(e) {
+        if (e && e.type) {
+          console.log(e.type);
+        } else {
+          console.log('DOMContentLoaded');
+        }
+        if (loadedMetrica) {
+          return;
+        }
+
+        (function(m, e, t, r, i, k, a) {
+          m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+          };
+          m[i].l = 1 * new Date();
+          k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })(window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+        ym(metricaId, "init", {
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true
+        });
+
+        loadedMetrica = true;
+        clearTimeout(timerId);
+
+        window.removeEventListener('scroll', loadMetrica);
+        window.removeEventListener('touchstart', loadMetrica);
+        document.removeEventListener('mouseenter', loadMetrica);
+        document.removeEventListener('click', loadMetrica);
+        document.removeEventListener('DOMContentLoaded', loadFallback);
+      }
+    })()
   </script>
   <noscript>
-    <div><img src="https://mc.yandex.ru/watch/72285235" style="position:absolute; left:-9999px;" alt=""/></div>
+    <div><img src="https://mc.yandex.ru/watch/72285235" style="position:absolute; left:-9999px;" alt="" /></div>
   </noscript>
   <!-- /Yandex.Metrika counter -->
   <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -61,4 +102,5 @@
   </script>
   @endif
 </body>
+
 </html>
