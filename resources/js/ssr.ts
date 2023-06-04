5e062dd96e876c12e84c335d5c00600ead7e6987
MikhailSergeev <michaelmorhant@gmail.com>
2023-06-04 22:18:48 +0300

    Fix pages path in ssr config.

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -3,14 +3,13 @@ import { createSSRApp, h } from "vue";
 import { renderToString } from "@vue/server-renderer";
 import { createInertiaApp } from "@inertiajs/vue3";
 import createServer from "@inertiajs/vue3/server";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
 createServer((page) =>
   createInertiaApp({
     page,
     render: renderToString,
     resolve: (name) => {
-      const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+      const pages = import.meta.glob("./Pages/*/*.vue", { eager: true });
       return pages[`./Pages/${name}.vue`];
     },
     setup({ App, props, plugin }) {
