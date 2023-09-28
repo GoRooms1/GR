3ebe5d1422362e405efc4f2e25f16e27269c7129
MikhailSergeev <michaelmorhant@gmail.com>
2023-09-28 10:30:13 +0300

    Change a teg to inertia link.

--- resources/js/components/ui/ArticleCard.vue
+++ resources/js/components/ui/ArticleCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="block w-full md:w-1/2 lg:w-1/3 xl:w-1/4 my-4 px-2 group"  :href="'/blog/' + article?.slug">
+  <Link class="block w-full md:w-1/2 lg:w-1/3 xl:w-1/4 my-4 px-2 group"  :href="'/blog/' + article?.slug">
         <div class="overflow-hidden object-cover rounded-tl-2xl rounded-tr-2xl h-52 w-full relative">
             <img class="w-full h-full object-cover" :src="article?.image?.conversions?.card ?? article?.image?.url" alt="img">
             <div class="absolute left-0 top-0 bg-[#6170FF] text-white flex flex-col justify-center text-center w-12 rounded-tl-2xl rounded-br-2xl">
@@ -14,11 +14,16 @@
             </div>
             <span class="font-bold text-sm leading-6 group-hover:underline text-[#6170FF] mt-auto">Подробнее</span>
         </div>
-    </a>
+    </Link>
 </template>
 
 <script>
+import { Link } from "@inertiajs/vue3";
+
 export default {
+  components: {
+    Link,
+  },
   props: {
     article: Object, 
   }
