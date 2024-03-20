90c17c01f48be2f89b6c34ceb61e2c3b12f83328
MikhailSergeev <michaelmorhant@gmail.com>
2024-03-20 21:25:35 +0300

    Translate verification mail.

--- lang/ru/messages.php
+++ lang/ru/messages.php
@@ -7,4 +7,9 @@
 
 return [
     'Regards' => 'С уважением',
+    'Hello!' => 'Приветствуем!',
+    'Verify Email Address' => 'Подтвердите адрес электронной почты',
+    'Please click the button below to verify your email address.' => 'Пожалуйста, нажмите на кнопку ниже, чтобы подтвердить свой адрес электронной почты.',
+    'If you did not create an account, no further action is required.' => 'Если вы не создавали учетную запись, никаких дальнейших действий не требуется.',
+    "If you're having trouble clicking the :actionText button, copy and paste the URL below\ninto your web browser:" => "Если у вас возникли проблемы с нажатием кнопки :actionText действия, скопируйте и вставьте приведенный ниже URL-адрес\nв свой веб-браузер:",
 ];
